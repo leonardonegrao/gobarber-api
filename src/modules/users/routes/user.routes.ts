05c18e3077bef437fe5ff9d08698be1b4a8be111
Leonardo Negrão <leonardonegrao@pop-os.localdomain>
2020-11-30 12:02:03 -0300

    implements users' avatars creation and update

--- src/modules/users/routes/user.routes.ts
+++ src/modules/users/routes/user.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from '@/config/upload'
 import CreateUserService from '@/users/services/CreateUserService'
+import UpdateUserAvatarService from '@/users/services/UpdateUserAvatarService'
+import ensureAuthenticated from '@/middlewares/ensureAuthenticated'
 
 const usersRouter = Router()
+const upload = multer(uploadConfig)
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -23,5 +28,24 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ message: err.message })
   }
 })
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService()
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFileName: req.file.filename,
+      })
+      return res.json({ user })
+    } catch (err) {
+      return res.status(400).json({ error: err.message })
+    }
+  }
+)
 
 export default usersRouter
