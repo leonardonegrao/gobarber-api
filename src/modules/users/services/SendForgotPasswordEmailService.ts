67870420f51cc028e89497331fc5aa932ba21eb7
Leonardo Negrão <leonardolnegrao@gmail.com>
2021-01-12 17:09:18 -0300

    creates forgot password service test and fakes

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,28 @@
+import { injectable, inject } from 'tsyringe'
+
+import User from '@/users/infra/typeorm/entities/User'
+import AppError from '@/errors/AppError'
+
+import IUsersRepository from '../repositories/IUsersRepository'
+import IMailProvider from '@/shared/container/providers/MailProvider/models/IMailProvider'
+
+interface IRequest {
+  email: string
+}
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+    @inject('MailProvider')
+    private mailProvider: IMailProvider
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.'
+    )
+  }
+}
