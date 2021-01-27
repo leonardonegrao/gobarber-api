3f7bd176778b00933cc54919244c4b8c0b1024ae
Leonardo Negrão <leonardolnegrao@gmail.com>
2021-01-27 20:33:57 -0300

    adds mail template provider using handlebars

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,15 +1,17 @@
 import nodemailer, { Transporter } from 'nodemailer'
+import { injectable, inject } from 'tsyringe'
 import IMailProvider from '../models/IMailProvider'
+import ISendMailDTO from '../dtos/ISendMailDTO'
+import IMailTemplateProvider from '@/shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider'
 
-interface IMessage {
-  to: string
-  body: string
-}
-
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter
 
-  constructor() {
+  constructor(
+    @inject('MailTemplateProvider')
+    private mailTemplateProvider: IMailTemplateProvider
+  ) {
     nodemailer.createTestAccount().then(account => {
       const transporter = nodemailer.createTransport({
         host: account.smtp.host,
@@ -25,15 +27,23 @@ export default class EtherealMailProvider implements IMailProvider {
     })
   }
 
-  public async sendMail(to: string, body: string): Promise<void> {
-    const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
-      to,
-      subject: 'Recuperação de senha',
-      text: body,
+  public async sendMail({
+    from,
+    to,
+    subject,
+    templateData,
+  }: ISendMailDTO): Promise<void> {
+    await this.client.sendMail({
+      from: {
+        name: from?.name || 'Equipe GoBarber',
+        address: from?.email || 'equipe@gobarber.com.br',
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject: subject,
+      html: await this.mailTemplateProvider.parse(templateData),
     })
-
-    console.log(`Message sent: ${message.messageId}`)
-    console.log(`Preview URL: ${nodemailer.getTestMessageUrl(message)}`)
   }
 }
